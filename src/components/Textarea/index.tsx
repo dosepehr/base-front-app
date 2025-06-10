66c9c2e4f836c77d51d8cbc2f80dba805536a897
dosepehr <dosepehr@gmail.com>
2025-06-10 16:35:16 +0330

    add textarea component

---
+++ src/components/Textarea/index.tsx
@@ -0,0 +1,59 @@
+import React, { FC } from 'react';
+import { TextareaProps } from './textarea.type';
+import { Size } from '@/utils/types/components/size.type';
+import { Theme } from '@/utils/types/components/theme.type';
+import classNames from 'classnames';
+
+const Textarea: FC<TextareaProps> = ({
+    className,
+    theme = 'accent',
+    size = 'md',
+    icon,
+    placeholder,
+    ...rest
+}) => {
+    const sizeClasses: Record<Size, string> = {
+        xs: 'textarea-xs',
+        sm: 'textarea-sm',
+        md: 'textarea-md',
+        lg: 'textarea-lg',
+        xl: 'textarea-xl',
+    };
+    const themeClasses: Record<Theme, string> = {
+        accent: 'textarea-accent',
+        error: 'textarea-error',
+        info: 'textarea-info',
+        neutral: 'textarea-neutral',
+        primary: 'textarea-primary',
+        secondary: 'textarea-secondary',
+        success: 'textarea-success',
+        warning: 'textarea-warning',
+        default: '',
+    };
+    const classes = classNames(
+        'w-full textarea placeholder:mr-4',
+        {
+            [`${themeClasses[theme]}`]: theme,
+        },
+        {
+            [`${sizeClasses[size]}`]: size,
+        },
+        {
+            'pr-10': icon,
+        },
+        className
+    );
+    return (
+        <div className='relative'>
+            <textarea
+                placeholder={placeholder}
+                className={`${classes}`}
+                {...rest}
+            ></textarea>
+            <div className='absolute top-2 right-3'>{icon}</div>
+        </div>
+    );
+};
+
+export default Textarea;
+
---
+++ src/components/Textarea/textarea.type.ts
@@ -0,0 +1,10 @@
+import { ComponentBase } from "@/utils/types/components/component-base.type";
+import { TextareaHTMLAttributes } from "react";
+
+export type TextareaProps = Omit<
+  TextareaHTMLAttributes<HTMLTextAreaElement>,
+  "size"
+> &
+  ComponentBase & {
+    icon?: React.ReactNode;
+  };

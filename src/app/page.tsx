b55f47c1edd4849e064ea00eb17caf5005f19e61
dosepehr <dosepehr@gmail.com>
2025-06-10 14:45:36 +0330

    add collapse component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import Button from '@/components/Button';
 import Checkbox from '@/components/Checkbox';
+import Collapse from '@/components/Collapse';
 export default function Home() {
     return (
         <div>
             <Checkbox defaultChecked />
             <Button>fk</Button>
+            <Collapse content='ff' key={'d'} title='lldlld' />
         </div>
     );
 }

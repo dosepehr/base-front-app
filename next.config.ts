64840c6563b0a1ae7feb72c24c447d0f1822deaf
dosepehr <dosepehr@gmail.com>
2025-06-10 14:14:54 +0330

    add svgr

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,51 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    /* config options here */
+    webpack(config) {
+        config.module.rules.push({
+            test: /\.svg$/,
+            use: [
+                {
+                    loader: '@svgr/webpack',
+                    options: {
+                        icon: true,
+                    },
+                },
+            ],
+        });
+        return config;
+    },
+    experimental: {
+        turbo: {
+            rules: {
+                '*.svg': {
+                    loaders: ['@svgr/webpack'],
+                    as: '*.js',
+                },
+            },
+        },
+    },
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'irantogermany-api.liara.run',
+            },
+        ],
+    },
+    typescript: {
+        // !! WARN !!
+        // Dangerously allow production builds to successfully complete even if
+        // your project has type errors.
+        // !! WARN !!
+        ignoreBuildErrors: true,
+    },
+    eslint: {
+        // Warning: This allows production builds to successfully complete even if
+        // your project has ESLint errors.
+        ignoreDuringBuilds: true,
+    },
 };
-
 export default nextConfig;
+

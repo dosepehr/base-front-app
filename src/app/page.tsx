912830480b427765617875cd91943f4b50379c5d
dosepehr <dosepehr@gmail.com>
2025-06-10 14:31:59 +0330

    add button component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,14 @@
 import Arrow from '@/../public/icons/arrow.svg';
+import Button from '@/components/Button';
 export default function Home() {
     return (
         <div>
             <p>متن فارسی</p>
             <p lang='en'>en content</p>
-            <Arrow />
+            <Arrow />{' '}
+            <Button variant='soft' theme='primary' size='lg'>
+                Soft Primary Large
+            </Button>
         </div>
     );
 }

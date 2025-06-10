cef88ef330f93f7cd4a790d742f51c85c0516cb6
dosepehr <dosepehr@gmail.com>
2025-06-10 14:14:44 +0330

    add daisyui colors

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
+import Arrow from '@/../public/icons/arrow.svg';
 export default function Home() {
     return (
         <div>
             <p>متن فارسی</p>
             <p lang='en'>en content</p>
+            <Arrow />
         </div>
     );
 }

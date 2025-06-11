5ffae3c68d82cd595cb0bcb6c005baafb352cace
dosepehr <dosepehr@gmail.com>
2025-06-11 14:31:39 +0330

    edit Select component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,24 @@
-import Input from '@/components/Input';
+import Select from '@/components/Select';
+import { OptionsType } from '@/components/Select/select.type';
 export default function Home() {
+    const s: OptionsType = {
+        options: [
+            {
+                title: '1',
+                value: '',
+            },
+            {
+                title: '2',
+                value: '2',
+            },
+        ],
+        title: 'pick',
+    };
     return (
         <div>
             <p className='text-white text-center'>خوش آمدید!</p>
-            <Input placeholder='ff' isAnimated  />
+            {/* <Input placeholder='ff' isAnimated /> */}
+            <Select options={s} />
         </div>
     );
 }
--- src/components/Select/index.tsx
+++ src/components/Select/index.tsx
@@ -1,8 +1,7 @@
-import { Size } from '@/utils/types/components/size.type';
-import { Theme } from '@/utils/types/components/theme.type';
 import React, { FC } from 'react';
 import { SelectProps } from './select.type';
 import classNames from 'classnames';
+import { Size, Theme } from '@/utils/types/components/component-base.type';
 
 const sizeClasses: Record<Size, string> = {
     xs: 'select-xs',
@@ -25,7 +24,7 @@ const themeClasses: Record<Theme, string> = {
 const Select: FC<SelectProps> = ({
     options,
     className,
-    theme = 'accent',
+    theme = 'primary',
     size = 'md',
     ...rest
 }) => {
@@ -40,10 +39,9 @@ const Select: FC<SelectProps> = ({
         className
     );
     return (
-        <select className={classes} {...rest}>
-            <option disabled={true} value={options.title}>
-                {options.title}
-            </option>
+        <select defaultValue={options.title} className={classes} {...rest}>
+            <option disabled={true}>{options.title}</option>
+
             {options.options.map((opt, i) => (
                 <option key={i} value={opt.value}>
                     {opt.title}
@@ -54,4 +52,5 @@ const Select: FC<SelectProps> = ({
 };
 
 export default Select;
+
 

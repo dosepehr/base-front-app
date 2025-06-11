5fe3f2221a8879573ae44465e8adc466ea07b955
dosepehr <dosepehr@gmail.com>
2025-06-11 14:57:59 +0330

    add fileInput component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,7 @@
-import Select from '@/components/Select';
-import { OptionsType } from '@/components/Select/select.type';
 export default function Home() {
-    const s: OptionsType = {
-        options: [
-            {
-                title: '1',
-                value: '',
-            },
-            {
-                title: '2',
-                value: '2',
-            },
-        ],
-        title: 'pick',
-    };
     return (
         <div>
             <p className='text-white text-center'>خوش آمدید!</p>
-            {/* <Input placeholder='ff' isAnimated /> */}
-            <Select options={s} />
         </div>
     );
 }

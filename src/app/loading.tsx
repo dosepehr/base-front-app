528c10ea74058f40d7c5abc02be5f260f281741a
dosepehr <dosepehr@gmail.com>
2025-06-11 13:48:15 +0330

    add loading page

---
+++ src/app/loading.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Spinner = () => {
+    return (
+        <div>
+            <p className='text-white text-center'>404</p>
+        </div>
+    );
+};
+
+export default Spinner;
+

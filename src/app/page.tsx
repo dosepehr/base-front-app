3a53791ffe58cf2beda63c04553eb61b7c4b1aa4
dosepehr <dosepehr@gmail.com>
2025-06-12 08:03:28 +0330

    add Tooltip component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,17 @@
+import Tooltip from '@/components/Tooltip';
+
 export default function Home() {
     return (
         <div>
             <p className='text-white text-center'>خوش آمدید!</p>
+
+            <br />
+            <br />
+            <br />
+            <br />
+            <Tooltip isOpen position='left' theme='info' content={'flfl'}>
+                <p>kkff</p>
+            </Tooltip>
         </div>
     );
 }

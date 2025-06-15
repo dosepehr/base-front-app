e4e4ddb7d647909d594a6235558da9f1e67c049b
dosepehr <dosepehr@gmail.com>
2025-06-15 09:13:40 +0330

    add collapse stories

--- src/components/Collapse/index.tsx
+++ src/components/Collapse/index.tsx
@@ -3,7 +3,7 @@ import { CollapseProps } from './collapse.type';
 
 const Collapse: FC<CollapseProps> = ({ title, content }) => {
     return (
-        <div className='collapse collapse-arrow bg-primary border border-gray-3 text-gray-3'>
+        <div className='collapse collapse-arrow border'>
             <input type='radio' name='my-accordion-2' />
             <div className='collapse-title font-semibold'>{title}</div>
             <div className='collapse-content text-sm'>{content}</div>

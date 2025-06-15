239508438f54ddbb74c70ec58584d7dbfa2cd5de
dosepehr <dosepehr@gmail.com>
2025-06-15 07:58:18 +0330

    edit Breadcrumb component

--- src/components/Breadcrumb/index.tsx
+++ src/components/Breadcrumb/index.tsx
@@ -4,7 +4,7 @@ import { BreadcrumbProps } from './breadcrumb.type';
 
 const Breadcrumb = ({ breadcrumb }: { breadcrumb: BreadcrumbProps[] }) => {
     return (
-        <div className='text-black font-bold text-xl mb-12 flex items-center justify-between'>
+        <div className='text-primary font-bold text-xl mb-12 flex items-center justify-between'>
             <div className='flex gap-x-1'>
                 <Link href='/'>Home</Link>
                 <p>/</p>

a9468147af0f9ce3720a309d82d2972637d9f634
dosepehr <dosepehr@gmail.com>
2025-07-26 10:28:50 +0330

    edit Dnd component

--- src/components/Dropzone/dropzone.type.ts
+++
@@ -1,8 +0,0 @@
-import { DropzoneOptions } from 'react-dropzone';
-
-export type DropzoneProps = DropzoneOptions & {
-    onChange: (file: File | null) => void;
-    showDetails?: boolean;
-    title: string;
-};
-

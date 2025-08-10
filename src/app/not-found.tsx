a3c87ecd82bf33a65bc1a715d1b04b85e31a7301
dosepehr <dosepehr@gmail.com>
2025-08-10 11:52:26 +0330

    edit not found page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,12 +1,9 @@
 import React from 'react';
 
-const NotFound = () => {
+export default function NotFound() {
     return (
         <div>
             <p className='text-white text-center'>404</p>
         </div>
     );
-};
-
-export default NotFound;
-
+}

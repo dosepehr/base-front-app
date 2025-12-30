2feee95bae815e8d3d56d6fb864aa92df0db6bd5
dosepehr <dosepehr@gmail.com>
2025-12-30 21:21:18 +0330

    update next js config for next 16

--- next.config.ts
+++ next.config.ts
@@ -1,28 +1,7 @@
-import { NextConfig } from 'next';
-import withPWA from 'next-pwa';
+import type { NextConfig } from "next";
 
-const isDev = process.env.NODE_ENV === 'development';
+const nextConfig: NextConfig = {
+  /* config options here */
+};
 
-const nextConfig = {
-    images: {
-        remotePatterns: [
-            {
-                protocol: 'https',
-                hostname: 'example.com',
-            },
-        ],
-    },
-    typescript: {
-        ignoreBuildErrors: true,
-    },
-    eslint: {
-        ignoreDuringBuilds: true,
-    },
-} satisfies NextConfig;
-
-export default withPWA({
-    dest: 'public',
-    register: true,
-    skipWaiting: true,
-    disable: isDev,
-})(nextConfig);
+export default nextConfig;

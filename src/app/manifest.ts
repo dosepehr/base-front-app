f1b742e6fd7f328469945ce3ebf57c709e0bdd40
dosepehr <dosepehr@gmail.com>
2026-01-01 15:33:25 +0330

    add pwa icons

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -6,20 +6,32 @@ export default function manifest(): MetadataRoute.Manifest {
         short_name: 'sudo sepehr',
         icons: [
             {
-                src: '/web-app-manifest-192x192.png',
+                src: '/pwa/icons/web-app-manifest-192x192.png',
                 sizes: '192x192',
                 type: 'image/png',
-                purpose: 'maskable',
             },
             {
-                src: '/web-app-manifest-512x512.png',
+                src: '/pwa/icons/web-app-manifest-512x512.png',
                 sizes: '512x512',
                 type: 'image/png',
-                purpose: 'maskable',
+            },
+            {
+                src: '/pwa/icons/web-app-manifest-144x144.png',
+                sizes: '144x144',
+                type: 'image/png',
             },
         ],
         theme_color: '#ffffff',
         background_color: '#ffffff',
         display: 'standalone',
+        start_url: '/',
+        screenshots: [
+            {
+                src: '/pwa/screenshots/1.jpg',
+                sizes: '1080x1920',
+                type: 'image/gif',
+                label: 'Application',
+            },
+        ],
     };
 }
